4e7cbc03f5b425e2a248099185329aa923d51986
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-01-15 13:24:02 +0100

    New classes Entity and Model

--- app/Database.php
+++ app/Database.php
@@ -7,32 +7,32 @@ use PDOException;
 
 class Database
 {
-    protected $db;
-    protected $host = 'localhost';
-    protected $dbname = 'blog';
-    protected $username = 'root';
-    protected $password = '';
+    static protected $_db;
+    static protected $host = 'localhost';
+    static protected $dbname = 'blog';
+    static protected $username = 'root';
+    static protected $password = '';
 
-    public function __construct()
-    {
-        $db = $this->getDb();
-        return $db;
-    }
 
     /**
      * Get $db link with PDO (create a NEW PDO instance if needed)
+     * @return PDO pdo link;
      */
-    public function getDb()
+    public static function getDb()
     {
-        if ($this->db === NULL) {
+        if (SELF::$_db === NULL) {
+            $host = SELF::$host;
+            $dbname = SELF::$dbname;
+            $username = SELF::$username;
+            $password = SELF::$password;
             try {
-                $db = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+                $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die("Database connection failed: " . $e->getMessage());
             }
         } else {
-            $db = $this->db;
+            $db = SELF::$_db;
         }
         return $db;
     }
