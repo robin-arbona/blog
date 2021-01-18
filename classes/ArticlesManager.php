906b40fe033a86b6ecdd4cb391f3e65eea556fba
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-01-18 17:56:51 +0100

    Articles without categories selection is available

--- classes/ArticlesManager.php
+++ classes/ArticlesManager.php
@@ -2,4 +2,12 @@
 
 class ArticlesManager extends Manager
 {
+    public function getByXWithOffset(int $offset, int $X = 5)
+    {
+        $SQL = "SELECT * FROM {$this->tableName} ORDER BY id ASC LIMIT " . (int) $X . " OFFSET " . ((int) $offset);
+        $sth = $this->db->query($SQL);
+        $sth->setFetchMode(PDO::FETCH_CLASS, $this->entityName);
+
+        return $sth->fetchAll();
+    }
 }
--- view/articles.php
+++ view/articles.php
@@ -11,3 +11,50 @@
  * ex : https://localhost/blog/articles.php/?categorie=1&start=10 affiche les
  * articles 11 à 15 ayant comme id_categorie 1).
  */
+
+require '../classes/Manager.php';
+require '../classes/ArticlesManager.php';
+require '../classes/ArticlesEntity.php';
+
+if (isset($_GET['start'])) {
+    $offset = $_GET['start'];
+} else {
+    $offset = 0;
+}
+
+$articlesManager = new ArticlesManager;
+$articles = $articlesManager->getByXWithOffset($offset, 5);
+
+require_once('template/header.php');
+
+?>
+
+<div class="container">
+    <h1 class="display-1 mt-5 mb-0">Welcome in my blog,</h1>
+    <h2 class="display-4 mt-0">Subtitle goes here.</h2>
+    <div class="row align-items-start justify-content-center">
+        <?php
+        foreach ($articles as $article) { ?>
+            <div class="col row justify-content-center my-5">
+                <a href="article.php?id=<?= $article->id ?>" class="card m-3 no-text-decoration" style="width: 20rem;">
+                    <img class="centered-and-cropped" src="../public/image/article_mainpic_<?= $article->id ?>.jpg" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $article->title ?></h5>
+                        <p class="card-text"><?= $article->getExtract() ?></p>
+                        <p class="card-text"><small class="text-muted">Written the <?= $article->date ?></small></p>
+                    </div>
+                </a>
+            </div>
+        <?php
+        } ?>
+    </div>
+
+    <div class="row align-items-start justify-content-center">
+        <a class="page-link" href="index.php?page=articles&start=1">More</a>
+    </div>
+
+</div>
+<?php
+
+require_once('template/footer.php');
