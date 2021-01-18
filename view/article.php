c7fa2f8ebd6e2c77cbad9b10120c1c8c1dbef22c
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-01-18 11:00:51 +0100

    First article draft view

--- view/article.php
+++ view/article.php
@@ -1,10 +1,22 @@
 <?php
 
-
+$article = $data['article'];
 /** - Une page contenant un article et ses commentaires (article.php) :
  * Cette page permet de voir un article, l’ensemble des commentaires
  * associés et la possibilité d’en ajouter un nouveau. Il faut utiliser l’argument
  * GET “id” afin de sélectionner l’article souhaité.
  * ex : https://localhost/blog/article.php/?id=1
  * */
-var_dump($data);
+
+?>
+<img src="public/image/article_mainpic_<?= $article->id ?>.jpg" class="img-fluid" alt="...">
+<div class="container">
+    <h1><?= $article->title ?> </h1>
+    <h2>category <?= $article->id_categorie ?> user <?= $article->id_utilisateur ?></h2>
+
+    <h3><?= $article->date ?></h3>
+
+    <p><?= $article->article ?></p>
+
+
+</div>
