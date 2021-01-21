d9055ec63c986d552a23d98050124f735d805356
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-01-21 11:04:14 +0100

    footer edition

--- view/template/footer.php
+++ view/template/footer.php
@@ -7,35 +7,35 @@
 ?>
 
 <footer>
-    <nav class="navbar navbar-expand-lg bg-dark justify-content-around " >
-       <ul class="nav flex-column">
-            <li class="nav-item">
+   <nav class="navbar navbar-expand-lg bg-dark justify-content-around ">
+      <ul class="nav flex-column">
+         <li class="nav-item">
             <a class="nav-link" href="index.php">Acceuil</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="connexion.php">Connexion</a> <!-- Ou Profil si session true-->
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="inscription.php">Inscription</a> <!-- Ou Deconnexion si session true -->
-            </li>
-        </ul>
-        <ul class="nav flex-column">
-            <li class="nav-item">
-               <a class="nav-link" href="articles.php">Articles</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="">Commentaires</a>
-            </li>
-        </ul>
-        <ul class="nav flex-column">
-            <li class="nav-item">
-               <a class="nav-link " href="creer-article.php">Create Article</a> <!--Activer si droit-->
-            </li>
-            <li class="nav-item">
-               <a class="nav-link disabled" href="#">Admin</a> <!--Activer si droit-->
-            </li>
-        </ul>
-    </nav>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="connexion.php">Connexion</a> <!-- Ou Profil si session true-->
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="inscription.php">Inscription</a> <!-- Ou Deconnexion si session true -->
+         </li>
+      </ul>
+      <ul class="nav flex-column">
+         <li class="nav-item">
+            <a class="nav-link" href="articles.php">Articles</a>
+         </li>
+
+      </ul>
+      <ul class="nav flex-column">
+         <li class="nav-item">
+            <a class="nav-link " href="creer-article.php">Create Article</a>
+            <!--Activer si droit-->
+         </li>
+         <li class="nav-item">
+            <a class="nav-link disabled" href="#">Admin</a>
+            <!--Activer si droit-->
+         </li>
+      </ul>
+   </nav>
 </footer>
 
 </body>
